5a9112c8148c7e1031fdd3b680b51770c374c1a0
iemong <irie.s@note.jp>
2021-10-13 13:24:40 +0900

    first commit

---
+++ nuxt3-app/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- Remove this component to get started! -->
+      Page visits: {{ data }}
+    <NuxtWelcome />
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  async setup() {
+
+    const { data } = await useAsyncData('count', () => $fetch('/api/counter'))
+
+
+    return {
+      data
+    }
+  }
+}
+</script>
